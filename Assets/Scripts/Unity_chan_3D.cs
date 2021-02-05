810c8c58c9e113f60d6848d72e4f709c33bfa62b
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-02-05 22:35:38 +0900

    "first commit

---
+++ Assets/Scripts/Unity_chan_3D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unity_chan_3D : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+                                                        //Vector3 force = new Vector3(0.0f, 0.0f, 1.0f);
+                                                        // 力を設定
+                                                        //rb.AddForce(force);
+                                                        // 力を加える
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * 0.01f;
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        else if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+    }
+}
